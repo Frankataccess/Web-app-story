125175ac759e488967c5bd031b33ed8092ee9ada
frankataccess <frank.mccann.355@accesscreative.ac.uk>
2024-10-09 12:54:41 +0100

    innitial commit

---
+++ web-app-story/src/App.jsx
@@ -0,0 +1,15 @@
+import {useState,useEffect} from "react"
+
+const App = () => {
+  return(
+    <div style={{
+      marginLeft: '50%',
+      marginTop:'50%',
+      width:'30%'
+    }}>
+      <Box color="White" ></Box>
+    </div>
+  )
+}
+
+export default App;
---
+++ web-app-story/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    screens:{
+      sm:"480",
+      sml:"768",
+      md:"834",
+      mdl:"1024",
+      lg:"1280",
+      xl:"1440",
+    },
+    extend: {
+      fontFamily: {
+        heading: ["Kanit","serif"],
+        body: ["Archivo,serif"],
+      },
+      colors:{
+        smokey_black: '#0E0300',
+        ercu: '#A59C78',
+        celadon: '#B3D09C',
+        champagne_pink: '#EED3C1',
+        timberwolf: '#DCD1CA',
+      },
+    },
+  },
+  plugins: [],
+}
